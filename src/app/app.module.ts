6bc46e6938f2e6d90590aa6b2340571602f6b7b3
Widura Wijenayake <Widura.Wijenayake@cambio.lk>
2018-05-15 18:10:11 +0530

    Added two more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlert } from './success-alert/success-alert.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlert
   ],
   imports: [
     BrowserModule
