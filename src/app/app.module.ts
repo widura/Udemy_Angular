8356980f1c0e020cd6f642c60c7d74af0861076d
Wijitha <wijithapaw@gmail.com>
2018-05-18 23:31:34 +0530

    Data binding ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlert } from './success-alert/success-alert.component';
-
+import { FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,7 +17,8 @@ import { SuccessAlert } from './success-alert/success-alert.component';
     SuccessAlert
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
